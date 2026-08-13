c4a7f6708e9e9b91849e850839a439995d20ab31
Kellyklo <kellykloti@gmail.com>
2026-08-12 21:39:13 -0300

    aula dia 12/08/2026

--- Atividades/lista_atividades_02/atividade_03/main.py
+++ Atividades/lista_atividades_02/atividade_03/main.py
@@ -1,58 +1,31 @@
 import json
 import os
 
-# Nome do arquivo JSON onde os dados serão salvos
-aluno_json = "alunos.json"
+alunos = []
 
-# Tenta carregar dados existentes para não apagar o arquivo anterior
-try:
-    if os.path.exists(aluno_json):
-        with open(aluno_json, "r", encoding="utf-8") as f:
-            dados_alunos = json.load(f)
-    else:
-        dados_alunos = []
-except json.JSONDecodeError:
-    dados_alunos = []
+os.system("cls" if os.name == "nt" else "clear")
 
-# Laço para cadastrar os alunos
 while True:
-    nome = input("Digite o nome do aluno: ")
-    
-    # Recebe as 3 notas
-    nota1 = float(input("Digite a nota 1: "))
-    nota2 = float(input("Digite a nota 2: "))
-    nota3 = float(input("Digite a nota 3: "))
-    
-    # Calcula a média
-    media = (nota1 + nota2 + nota3) / 3
-    
-    # Verifica aprovação
-    if media >= 7:
-        situacao = "Aprovado"
-    else:
-        situacao = "Reprovado"
-        
-    # Mostra o resultado na tela
-    print(f"Média: {media:.2f} - Situação: {situacao}")
-    
-    # Cria o dicionário com os dados do aluno atual
-    aluno = {
-        "nome": nome,
-        "notas": [nota1, nota2, nota3],
-        "media": round(media, 2),
-        "situacao": situacao
-    }
-    
-    # Adiciona na lista geral
-    dados_alunos.append(aluno)
-    
-    # Pergunta se deseja continuar
-    continuar = input("Deseja inserir notas de outro aluno? (s/n): ").strip().lower()
-    if continuar != 's':
-        break
+    print("1 - Informa dados")
+    print("2 - sair do programa")
+    opcao = input("Informe a opcao: ").strip()
+    os.system("cls" if os.name == "nt" else "clear")
+    match opcao:
+        case "1":
+            aluno = {}
+            notas = [0,0,0]
 
-# Salva a lista completa no arquivo JSON
-with open(aluno_json, "w", encoding="utf-8") as f:
-    json.dump(dados_alunos, f, ensure_ascii=False, indent=4)
-
-print(f"Dados salvos com sucesso em '{aluno_json}'!")
+            aluno['nome'] = input("Informe o nome do aluno: ").strip().title()
+            for i in range(len(notas)):
+                notas[i] = float(input("informe {i+1}ª nota: ").replace(",","."))
+            aluno['notas'] = notas
+            aluno['media'] = sum(notas)/len(notas)
+            aluno['resultado'] = "aprovado" if aluno['média'] >=7 else "reprovado"
+            alunos.append(aluno)
+            with open("atividade_03/arquivo.json","w",encoding="utf-8")as f:
+                json.dump(alunos, f)
+        case "2":
+            break
+        case _:
+           print("Opção invalida.")
+           continue 
