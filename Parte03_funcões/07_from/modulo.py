d67cb735b9516df5ab91a2f7eb5227f0a5271c7b
Kellyklo <kellykloti@gmail.com>
2026-08-14 21:25:37 -0300

    aula de hoje 14/08/2026

---
+++ Parte03_funcões/07_from/modulo.py
@@ -0,0 +1,17 @@
+import os
+
+def limpa():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def somar(x, y):
+    return x+y
+
+def subitrair(x, y):
+    return x-y
+
+def multiplicar(x, y):
+    return x*y
+
+def dividir(x, y):
+    return x/y
+  
