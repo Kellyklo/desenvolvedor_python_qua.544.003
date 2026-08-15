8a5db5f6d122b8caee842b832467c49ae903f8dc
Kellyklo <kellykloti@gmail.com>
2026-08-14 21:33:06 -0300

    aula do dia 14/08/2026

---
+++ Parte03_funcões/10_lambda/main.py
@@ -0,0 +1,9 @@
+somar = lambda x, y: x+y
+
+def main():
+    x = int(input("informe o valor de X:"))
+    y = int(input("informe o valor de Y:"))
+    print(f"O valor da soma é {somar(x,y)}")
+
+if __name__ =="__main__":
+    main()
