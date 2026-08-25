3060ac37ffad9326e56dbfe3894096e57cd473b1
Kellyklo <kellykloti@gmail.com>
2026-08-24 21:37:41 -0300

    aula do dia 24/08/2026

---
+++ parte04-orientacao-a-objetivo/06_interface/main.py
@@ -0,0 +1,67 @@
+import os 
+import datetime
+
+from datetime import date
+
+
+from module import Conta
+
+def limpar():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def hoje():
+    return date.today().striftime("%d/%n/%Y")
+
+def agora():
+    return datetime.datetime.now().striftime("%H/%M/%S")
+
+def main():
+    cc = Conta(titular="",agencia="1234-5",n_conta="10123-4",saldo=0.0)
+
+    limpar()
+
+    cc.titular = input("Informe o nome do titular da conta: ").strip().title
+    cc.cpf = input("Informe o CPF do titular da conta: ").strip()
+
+    limpar()
+    print(f"Conta criada no dia {hoje()} às {agora()}.")
+
+    while True:
+        print("0 - Sair do programa")
+        print("1 - Consultar dados da conta")
+        print("2 - Fazer depósito")
+        print("3 - Fazer saque")
+        opcao = input("Informe a opcao desejada: ").strip()
+        limpar()
+        match opcao:
+            case "0":
+                break
+            case "1":
+                print(f"Data da consulta: {hoje()}")
+                print(f"Hora da consulta: {agora()}")
+                cc.consultar_conta()
+                continue
+            case "2":
+                valor = float(input("Informe o valor a ser depositado: R$ ").replace (",","."))
+                if valor >= 0:
+                    print(f"Deposito efetuado com sucesso, às {agora()} do dia {hoje()}.")
+                    print(f"Saldo atural: R$ {cc.fazer_deposito(valor):.2f}")
+                    continue
+            case "3":
+                Valor = float(input("Informe o valor do saque : R$ "). replace(",","."))
+                if valor >= 0:
+                    if valor <= cc.saldo:
+                        print(f"Saque efetuado com sucesso às {agora()} do dia {hoje()}")
+                        print(f"saldo atual: R$ {cc.fazer_saque(valor):.2f}")
+                    else:
+                        print("Saldo isuficiente.")
+                else:
+                    print("Valor não pode ser sacado.")
+                    continue
+            case _:
+                print("Opção invalida.")
+                continue
+
+
+if __name__ == "__main__":
+    main()
