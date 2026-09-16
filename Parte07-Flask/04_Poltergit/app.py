67ad48ec1583e4695020e7d50dda510de85e4b4d
Kellyklo <kellykloti@gmail.com>
2026-09-16 20:59:26 -0300

    Commit do dia 16/09/m2026

---
+++ Parte07-Flask/04_Poltergit/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import pyautogui as auto
+
+from datetime import date
+
+app = Flask (__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/commitar", methods = ['GET','POST'] )
+def commitar():
+    hoje = date.today().strftime("%d/%m/m%Y")
+    msg = None
+    repositorio = None
+    if request.method == "POST":
+        repositorio = request.form.get("repositorio","")
+    if repositorio:
+        auto.PAUSE = 1
+        auto.hotkey("Win","r")
+        auto.write("cmd")
+        auto.press("enter")
+        auto.write(f"cd {repositorio}")
+        auto.press("enter")
+        auto.write("git add .")
+        auto.press("enter")
+        auto.write(f'git commit -m "Commit do dia {hoje}"')
+        auto.press("enter")
+        auto.write("git push")
+        auto.press("enter")
+        auto.sleep(3)
+        auto.Write("exit")
+        auto.press("enter")
+    return render_template("index.html")
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
