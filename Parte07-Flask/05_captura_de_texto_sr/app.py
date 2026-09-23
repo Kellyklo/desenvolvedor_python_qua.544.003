b4d6fefded214cd8ef4ca9a1a8cf463f697fce9e
Kellyklo <kellykloti@gmail.com>
2026-09-22 21:32:04 -0300

    Commit do dia 22/09/2026

--- Parte07-Flask/05_captura_de_texto_sr/app.py
+++ Parte07-Flask/05_captura_de_texto_sr/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, send_file
+from translate import Translator
 import easyocr
 
 import io
@@ -27,8 +28,21 @@ def exportar_texto():
     if request.method == 'POST':
         texto = request.form.get("texto", "")
         arquivo_buffer = io.BytesIO(texto.encode('utf-8'))
-        return send_file(arquivo_buffer, mimetype= 'text/plain', as_attachment=True, download_name='texto_extraido.txt')
+        return send_file(arquivo_buffer, 
+                        mimetype= 'text/plain',
+                        as_attachment=True, 
+                        download_name='texto_extraido.txt'
+                        )
     return render_template("exportar_sucesso.html")
+
+@app.route("/traducao", methods = ['POST'])
+def traduzir():
+    tradutor = Translator(to_lang="pt")
+    if request.method == "POST":
+        texto = request.form.get("texto", "")
+        texto_traduzido = tradutor.translate(texto)
+        return render_template("extracao.html", texto=texto_traduzido)
+    return render_template("extracao.htmal",texto="")
 
 
 if __name__ == "__main__":
